53edfd0b5a175d8bf03ba766f686d929df916939
Anders Jonsson <aj224rj@student.lnu.se>
2022-03-07 14:00:48 +0100

    Set up folders and initial files, includei user-model

---
+++ src/models/user.js
@@ -0,0 +1,81 @@
+/**
+ * Mongoose model User.
+ *
+ * @author Anders Jonsson
+ * @version 1.0.0
+ */
+
+import bcrypt from 'bcrypt'
+import mongoose from 'mongoose'
+import validator from 'validator'
+
+const { isEmail } = validator
+
+// Create a schema.
+const schema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Email address is required.'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [isEmail, 'Please provide a valid email address.']
+  },
+  password: {
+    type: String,
+    minLength: [10, 'The password must be of minimum length 10 characters.'],
+    maxLength: [256, 'The password must be of maximum length 256 characters.'],
+    required: [true, 'Password is required.']
+  },
+  permissionLevel: Number
+}, {
+  timestamps: true,
+  toJSON: {
+  /**
+   * Performs a transformation of the resulting object to remove sensitive information.
+   *
+   * @param {object} doc - The mongoose document which is being converted.
+   * @param {object} ret - The plain object representation which has been converted.
+   */
+    transform: function (doc, ret) {
+      delete ret._id
+      delete ret.__v
+    },
+    virtuals: true // ensure virtual fields are serialized
+  }
+})
+
+schema.virtual('id').get(function () {
+  return this._id.toHexString()
+})
+
+// Salts and hashes password before save.
+schema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next()
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12)
+})
+
+/**
+ * Authenticates a user.
+ *
+ * @param {string} email - Users email.
+ * @param {string} password - Users password.
+ * @returns {Promise<User>} - Promise for the user-object from db.
+ */
+schema.statics.authenticate = async function (email, password) {
+  const user = await this.findOne({ email })
+
+  // If no user found or password is wrong, throw an error.
+  if (!(await bcrypt.compare(password, user?.password))) {
+    throw new Error('Invalid credentials.')
+  }
+
+  // User found and password correct, return the user.
+  return user
+}
+
+// Create a model using the schema.
+export const User = mongoose.model('User', schema)
