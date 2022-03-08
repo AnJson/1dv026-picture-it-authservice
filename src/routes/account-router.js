10d4adbec129988b6e017e3078216ff59cd52a99
Anders Jonsson <aj224rj@student.lnu.se>
2022-03-08 18:16:03 +0100

    Revert "Implement version-handeling by route-structure and folder-structure"
    This reverts commit bd0e9bbf69f7e3ebd54f3e84df1d48cb3932e67a.

--- src/routes/account-router.js
+++ src/routes/account-router.js
@@ -6,12 +6,16 @@
  */
 
 import express from 'express'
-import { AccountController } from '../../../controllers/api/account-controller.js'
+import { AccountController } from '../controllers/account-controller.js'
 
 export const router = express.Router()
 
 const controller = new AccountController()
 
+// Map HTTP verbs and route paths to controller actions.
+
+// Log in
 router.post('/login', (req, res, next) => controller.login(req, res, next))
 
+// Register
 router.post('/register', (req, res, next) => controller.register(req, res, next))
--- src/routes/router.js
+++ src/routes/router.js
@@ -7,11 +7,12 @@
 
 import express from 'express'
 import createError from 'http-errors'
-import { router as v1Router } from './api/v1/router.js'
+import { router as accountRouter } from './account-router.js'
 
 export const router = express.Router()
 
-router.use('/api/v1', v1Router)
+router.get('/', (req, res) => res.json({ message: 'Welcome to the auth-service!' }))
+router.use('/', accountRouter)
 
 // Catch 404 (ALWAYS keep this as the last route).
 router.use('*', (req, res, next) => next(createError(404)))
